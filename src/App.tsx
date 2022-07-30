a14adf8a308eea9842ff08a37578a3c90fba8e0e
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-30 16:09:04 -0300

    communication between different components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -20,13 +22,11 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-      <Modal
+      <Dashboard />
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
-      <Dashboard />
+      />
       <GlobalStyle />
     </>
   );
